06b61584bb67d486939651d7623bced127fb4d88
SajitPanicker <102275277+SajitPanicker@users.noreply.github.com>
2025-11-14 17:22:01 +0530

    Add files via upload

---
+++ utils/technicals.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def calculate_technicals(hist):
+    hist["SMA20"] = hist["Close"].rolling(20).mean()
+    hist["SMA50"] = hist["Close"].rolling(50).mean()
+    hist["SMA200"] = hist["Close"].rolling(200).mean()
+
+    delta = hist["Close"].diff()
+    gain = delta.where(delta > 0, 0).rolling(14).mean()
+    loss = -delta.where(delta < 0, 0).rolling(14).mean()
+    rs = gain / loss
+    hist["RSI"] = 100 - (100 / (1 + rs))
+
+    return hist
